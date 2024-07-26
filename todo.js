f60ca33dc0ed79f5ba20a5d596db2f7229043958
Myles <mylesvirgilcruz@gmail.com>
2024-07-25 17:40:04 -0700

    Design style for new tasks added

--- todo.js
+++ todo.js
@@ -1,12 +1,16 @@
 // Global Variables
 let tasks = document.querySelector('.tasks');
+let taskCount = 0;
 
 // Handle new task being created
 window.addEventListener('DOMContentLoaded', () => {
     document.getElementById('add').addEventListener('click', () => {
-        let input = document.querySelector('.new-task').value;
+        taskCount += 1;
         let newTask = document.createElement('div');
+        let input = document.querySelector('.new-task').value;
         newTask.textContent = input;
+        newTask.setAttribute('class','task');
+        newTask.setAttribute('id','task-'+taskCount);
         tasks.appendChild(newTask);
     });
 });
