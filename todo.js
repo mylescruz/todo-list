d9eaa43fa9fb716e4b70cadc63eb2c2e6ac03029
Myles <mylesvirgilcruz@gmail.com>
2024-07-25 19:35:48 -0700

    Add checkbox to task

--- todo.js
+++ todo.js
@@ -7,7 +7,7 @@ window.addEventListener('DOMContentLoaded', () => {
     document.getElementById('add').addEventListener('click', () => {
         let input = document.querySelector('.new-task');
         
-        if (input.value !== "") {
+        if (/\S/.test(input.value)) {
             taskCount += 1;
             let newTask = document.createElement('div');
             
@@ -15,8 +15,11 @@ window.addEventListener('DOMContentLoaded', () => {
             newTask.setAttribute('class','task');
             newTask.setAttribute('id','task-'+taskCount);
             tasks.appendChild(newTask);
+            let checkBox = document.createElement('input');
+            checkBox.setAttribute('type','checkbox');
+            checkBox.setAttribute('class','check-box');
+            newTask.appendChild(checkBox);
+            input.value = "";
         }
-
-        input.value = "";
     });
 });
