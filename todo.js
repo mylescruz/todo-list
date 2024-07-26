e1f736000630943541e9c19dcdc2ef563ec3417c
Myles <mylesvirgilcruz@gmail.com>
2024-07-25 18:11:03 -0700

    Only allow a new task with input

--- todo.js
+++ todo.js
@@ -5,12 +5,18 @@ let taskCount = 0;
 // Handle new task being created
 window.addEventListener('DOMContentLoaded', () => {
     document.getElementById('add').addEventListener('click', () => {
-        taskCount += 1;
-        let newTask = document.createElement('div');
-        let input = document.querySelector('.new-task').value;
-        newTask.textContent = input;
-        newTask.setAttribute('class','task');
-        newTask.setAttribute('id','task-'+taskCount);
-        tasks.appendChild(newTask);
+        let input = document.querySelector('.new-task');
+        
+        if (input.value !== "") {
+            taskCount += 1;
+            let newTask = document.createElement('div');
+            
+            newTask.textContent = input.value;
+            newTask.setAttribute('class','task');
+            newTask.setAttribute('id','task-'+taskCount);
+            tasks.appendChild(newTask);
+        }
+
+        input.value = "";
     });
 });
