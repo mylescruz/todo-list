bd5be574e47915078a3ad19977bacbd33f5ec025
Myles <mylesvirgilcruz@gmail.com>
2024-07-26 09:48:04 -0700

    Add create task function and 'Enter' key functionality

--- todo.js
+++ todo.js
@@ -2,24 +2,35 @@
 let tasks = document.querySelector('.tasks');
 let taskCount = 0;
 
-// Handle new task being created
-window.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('add').addEventListener('click', () => {
-        let input = document.querySelector('.new-task');
+// Function to create task
+function createTask() {
+    let input = document.querySelector('.new-task');
         
-        if (/\S/.test(input.value)) {
-            taskCount += 1;
-            let newTask = document.createElement('div');
-            
-            newTask.textContent = input.value;
-            newTask.setAttribute('class','task');
-            newTask.setAttribute('id','task-'+taskCount);
-            tasks.appendChild(newTask);
-            let checkBox = document.createElement('input');
-            checkBox.setAttribute('type','checkbox');
-            checkBox.setAttribute('class','check-box');
-            newTask.appendChild(checkBox);
-            input.value = "";
+    if (/\S/.test(input.value)) {
+        taskCount += 1;
+        let newTask = document.createElement('div');
+        
+        newTask.textContent = input.value;
+        newTask.setAttribute('class','task');
+        newTask.setAttribute('id','task-'+taskCount);
+        tasks.appendChild(newTask);
+        let checkBox = document.createElement('input');
+        checkBox.setAttribute('type','checkbox');
+        checkBox.setAttribute('class','checkbox');
+        checkBox.setAttribute('id','checkbox-'+taskCount);
+        newTask.appendChild(checkBox);
+        input.value = "";
+    }
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+    // Handle new task being created
+    document.getElementById('add').addEventListener('click', createTask);
+
+    // Handle enter button pressed to add task
+    window.addEventListener('keydown', event => {
+        if (event.code === 'Enter') {
+            createTask();
         }
-    });
+    })
 });
